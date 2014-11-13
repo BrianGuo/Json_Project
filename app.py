9ae4c700c5068271c533e4519dd2869f3cf8f458
Brian Guo <helen@Helens-MacBook-Air.local>
2014-11-13 13:17:38 -0500

    something

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-from flask import
+from flask import Flask, request, render_template, redirect
+import urllib2
+from urllib2 import urlopen
+cards = urlopen ("https://irythia-hs.p.mashape.com/cards")
+response = cards.read()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	with app.test_reqeust_context():
+		response = request.get("https://irythia-hs.p.mashape.com/cards", headers = {"X-Mashape-Key": "APP"})
+
+
+if __name__ == "__main__":
+	print response
