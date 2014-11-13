044f0fd2183e0c92781d6e185232430123b1c735
Brian <brianguosh@gmail.com>
2014-11-13 13:14:53 -0500

    "updated app"

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from flask import
-http://developer.nytimes.com/docs/books_api/Books_API_Best_Sellers
+from flask import Flask, request, render_template
+#http://developer.nytimes.com/docs/books_api/Books_API_Best_Sellers
 #New York Times Best Selling Books API
+import urllib2
+app = Flask(__name__)
+books = urlopen("http://api.nytimes.com/svc/books/v2/hardcover-fiction.json?&api-key=aed470a02daf0898f629d3784516e2d4:11:70183313")
+#response = books.read()
+
+if __name__ == "__main__":
+    print response
